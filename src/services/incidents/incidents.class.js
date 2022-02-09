202cdb9ee7f436617990736ce2d2c289e72da920
fadholifh <fadholifh14@gmail.com>
2022-02-09 15:14:11 +0700

    fix created updated incident
    fix created updated incident

--- src/services/incidents/incidents.class.js
+++ src/services/incidents/incidents.class.js
@@ -1,5 +1,10 @@
 const { Service } = require('feathers-mongoose');
-
+const momentTz = require('moment-timezone');
 exports.Incidents = class Incidents extends Service {
+  async create(params) {
 
+    params.createdAt = momentTz().add(7, 'hours').format();
+    params.updatedAt = momentTz().add(7, 'hours').format();
+    return super.create(params);
+}
 };
