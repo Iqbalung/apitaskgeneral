7c07a3812e7780c288362625858cfd49405ecdc5
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-02-11 16:46:49 +0700

    add mutation_id query

--- src/services/incidents/incidents.class.js
+++ src/services/incidents/incidents.class.js
@@ -1,6 +1,14 @@
 const { Service } = require('feathers-mongoose');
 const momentTz = require('moment-timezone');
 exports.Incidents = class Incidents extends Service {
+  async find(params) {
+    if (params.query.mutation_id) {
+      params.query.refNumber = { $regex: new RegExp(params.query.mutation_id, 'i') }
+    }
+
+    return super.find(params)
+  }
+
   async create(params) {
 
     params.createdAt = momentTz().add(7, 'hours').format();
