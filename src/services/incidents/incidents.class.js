042d2c51bd45cf4c4080059f87ea8643f61ff8f3
fadholifh <fadholifh14@gmail.com>
2022-02-14 13:22:13 +0700

    search incident
    search incident

--- src/services/incidents/incidents.class.js
+++ src/services/incidents/incidents.class.js
@@ -1,10 +1,31 @@
 const { Service } = require('feathers-mongoose');
 const momentTz = require('moment-timezone');
 exports.Incidents = class Incidents extends Service {
+  async find(params) {
+    if (params.query['detailData.account_receiver']) {
+      params.query['detailData.account_receiver'] = { $regex: new RegExp(params.query['detailData.account_receiver'], 'i') };
+    } else {
+      delete params.query['detailData.account_number'];
+    }
+
+    if (params.query['detailData.account_sender']) {
+      params.query['detailData.account_sender'] = { $regex: new RegExp(params.query['detailData.account_sender'], 'i') };
+    } else {
+      delete params.query['detailData.bank_type'];
+    }
+
+    if (params.query['detailData.amount']) {
+      params.query['detailData.amount'] = parseInt(params.query['detailData.amount']);
+    } else {
+      delete params.query['detailData.amount'];
+    }
+
+    return super.find(params);
+  }
   async create(params) {
 
     params.createdAt = momentTz().add(7, 'hours').format();
     params.updatedAt = momentTz().add(7, 'hours').format();
     return super.create(params);
-}
+  }
 };
