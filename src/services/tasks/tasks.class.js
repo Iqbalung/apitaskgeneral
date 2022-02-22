fa2899b495635aebb847502948d0b8d1c0ed3f97
fadholifh <fadholifh14@gmail.com>
2022-02-22 15:14:55 +0700

    taskAssigne
    taskAssigne

--- src/services/tasks/tasks.class.js
+++ src/services/tasks/tasks.class.js
@@ -8,42 +8,48 @@ exports.Tasks = class Tasks extends Service {
               '$lte': new Date(params.query.createdAt.$lte).toISOString()
             }
         }
-    
+
         if (params.query.userId) {
             params.query.userId = { $regex: new RegExp(params.query.userId, 'i') }
         } else {
             delete params.query.userId
         }
-    
+
         if (params.query['taskData.account_number']) {
             params.query['taskData.account_number'] = { $regex: new RegExp(params.query['taskData.account_number'], 'i') }
         } else {
             delete params.query['taskData.account_number']
         }
-    
+
         if (params.query['taskData.anRekening']) {
             params.query['taskData.anRekening'] = { $regex: new RegExp(params.query['taskData.anRekening'], 'i') }
         } else {
             delete params.query['taskData.anRekening']
         }
-    
+
         if (params.query['taskData.bank_type']) {
             params.query['taskData.bank_type'] = { $regex: new RegExp(params.query['taskData.bank_type'], 'i') }
         } else {
             delete params.query['taskData.bank_type']
         }
-    
+
         if (params.query['taskData.amount']) {
             params.query['taskData.amount'] = parseInt(params.query['taskData.amount'])
         } else {
             delete params.query['taskData.amount']
         }
 
+        if (params.query['taskAssigne']) {
+          params.query['taskAssigne'] = parseInt(params.query['taskAssigne'])
+      } else {
+          delete params.query['taskAssigne']
+      }
+
         return super.find(params)
     }
 
     async create(params) {
-        
+
         params.createdAt = momentTz().add(7, 'hours').format()
       params.updatedAt = momentTz().add(7, 'hours').format()
         console.log("why",params);
