90da8952b2957ebe5d6ff7755f9f0f5ba5e554b9
ibrahim <ibrahimalanshor6@gmail.com>
2022-02-22 16:44:19 +0700

    fix user id and worker

--- src/services/tasks/tasks.class.js
+++ src/services/tasks/tasks.class.js
@@ -8,43 +8,49 @@ exports.Tasks = class Tasks extends Service {
               '$lte': new Date(params.query.createdAt.$lte).toISOString()
             }
         }
-    
+
         if (params.query.userId) {
             params.query['taskData.userId'] = { $regex: new RegExp(params.query.userId, 'i') }
             delete params.query.userId
         } else {
             delete params.query.userId
         }
-    
+
         if (params.query['taskData.account_number']) {
             params.query['taskData.account_number'] = { $regex: new RegExp(params.query['taskData.account_number'], 'i') }
         } else {
             delete params.query['taskData.account_number']
         }
-    
+
         if (params.query['taskData.anRekening']) {
             params.query['taskData.anRekening'] = { $regex: new RegExp(params.query['taskData.anRekening'], 'i') }
         } else {
             delete params.query['taskData.anRekening']
         }
-    
+
         if (params.query['taskData.bank_type']) {
             params.query['taskData.bank_type'] = { $regex: new RegExp(params.query['taskData.bank_type'], 'i') }
         } else {
             delete params.query['taskData.bank_type']
         }
-    
+
         if (params.query['taskData.amount']) {
             params.query['taskData.amount'] = parseInt(params.query['taskData.amount'])
         } else {
             delete params.query['taskData.amount']
         }
 
+        if (params.query['taskAssigne']) {
+          params.query['taskAssigne'] = { $regex: new RegExp(params.query['taskAssigne'], 'i') }
+        } else {
+            delete params.query['taskAssigne']
+        }
+
         return super.find(params)
     }
 
     async create(params) {
-        
+
         params.createdAt = momentTz().add(7, 'hours').format()
       params.updatedAt = momentTz().add(7, 'hours').format()
         console.log("why",params);
