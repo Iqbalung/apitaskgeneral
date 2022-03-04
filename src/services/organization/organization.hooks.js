6f55e95b69292316550ee53b2653f090247e8dc3
Ibrahim Al Anshor <ibrahimalanshor6@gmaile.com>
2022-03-04 20:15:42 +0700

    add organization

---
+++ src/services/organization/organization.hooks.js
@@ -0,0 +1,36 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+const {
+  hashPassword, protect
+} = require('@feathersjs/authentication-local').hooks;
+
+module.exports = {
+  before: {
+    all: [],
+    find: [],
+    get: [],
+    create: [authenticate('jwt'), hashPassword('password')],
+    update: [authenticate('jwt'), hashPassword('password')],
+    patch: [authenticate('jwt'), hashPassword('password')],
+    remove: [authenticate('jwt')]
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
